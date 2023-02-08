4791443c7c1cfffccfa6c6cfd913fe80dfbbcd18
Tris Doan <doanminhtri8183@gmail.com>
2023-02-08 17:14:12 +0700

    first commit

---
+++ academy/controllers/controllers.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+
+class Academy(http.Controller):
+    @http.route('/academy/academy', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
